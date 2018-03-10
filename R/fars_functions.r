f36dcda8d161456ff38006f7a302f3aac7feee23
Kolchin <skolchin@kpmg.com>
2018-03-10 21:19:38 +0300

    system.file() call added to make_filename() to refer to provided files

--- R/fars_functions.r
+++ R/fars_functions.r
@@ -43,7 +43,8 @@ fars_read <- function(filename) {
 #' @export
 make_filename <- function(year) {
         year <- as.integer(year)
-        sprintf("accident_%d.csv.bz2", year)
+        filename <- sprintf("accident_%d.csv.bz2", year)
+        system.file('extdata', filename, package = 'farspkg')
 }
 
 #' Read FARS data for multiple years
